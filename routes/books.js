a65269fadcc31f0b7bc86dfeda6d3ee3976d2398
Rhonda Lawrence <rhonda.lawrence@verizon.com>
2019-08-23 16:56:03 -0500

    submit ready being commited

--- routes/books.js
+++ routes/books.js
@@ -8,22 +8,39 @@ router.use(bodyParser.urlencoded({ extended: false }));
 var sqlite3 = require('sqlite3');
 
 const Book = require("../models").Book;
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
-//Get the books listing, show books in decending order
+// Get the books listing, show books in decending order
 router.get('/', function (request, response, next) {
-    Book.findAll({order: [["createdAt", "DESC"]]}).then(function(books){
-      response.render("all_books", {title: "Books", books: books});
-    }).catch(function (err) {
-        console.log(err);
-        response.sendStatus(500);
+    let pg = request.query.page;
+
+    if (!pg)
+        pg = 1;
+
+    Book.findAll({
+        order: [["createdAt", "DESC"]]
+    }).then(function (totalBks) {
+        Book.findAll({
+            order: [["createdAt", "DESC"]],
+            offset: (pg * 8) - 8,
+            limit: 8
+        }).then(function (books) {
+            const totalPgs = Math.ceil(totalBks.length / 8);
+            response.render("all_books", { title: "Books", books: books, totalPgs: totalPgs });
+        }).catch(function (err) {
+            console.log(err);
+            response.sendStatus(500);
+        });
     });
 });
 
-// Get create a new book
+// Render the new book form
 router.get("/new", function (request, response, next) {
     response.render("new_book", { title: "New Book", book: Book.build() });
 });
 
+// Create the new book if there are no errors
 router.post('/new', function (request, response, next) {
     Book.create(request.body).then(function(book) {
          response.redirect("/");
@@ -42,7 +59,133 @@ router.post('/new', function (request, response, next) {
      });
 });
 
-//Get individual book
+// Route to perform a post search using the posted search values title, author, genre and year
+router.post('/search', function (request, response, next) {
+    let pg = request.query.page;
+
+    if (!pg)
+        pg = 1;
+
+    let searchCriteria = [];
+
+    if (request.body.title != "") {
+        searchCriteria.push({
+            title: {
+                [Op.like]: '%' + request.body.title + '%'
+            }
+        });
+    }
+
+    if (request.body.author != "") {
+        searchCriteria.push({
+            author: {
+                [Op.like]: '%' + request.body.author + '%'
+            }
+        });
+    }
+
+    if (request.body.genre != "") {
+        searchCriteria.push({
+            genre: {
+                [Op.like]: '%' + request.body.genre + '%'
+            }
+        });
+    }
+
+    if (request.body.year != "") {
+        searchCriteria.push({year: request.body.year});
+    }
+
+    Book.findAll({
+        order: [["createdAt", "DESC"]],
+        where: {
+            [Op.and]: searchCriteria
+        }
+    }).then(function (totalSearchBks) {
+        Book.findAll({
+            order: [["createdAt", "DESC"]],
+            where: {
+                [Op.and]: searchCriteria
+            },
+            offset: (pg * 8) - 8,
+            limit: 8
+        }).then(function (books) {
+            const totalPgs = Math.ceil(totalSearchBks.length / 8);
+            response.render("search", {
+                title: "Books", books: books, totalPgs: totalPgs,
+                searchTitle: request.body.title, searchAuthor: request.body.author, searchGenre: request.body.genre,
+                searchYear: request.body.year});
+        }).catch(function (err) {
+            console.log(err);
+            response.sendStatus(500);
+        });
+    });
+});
+
+// Route to perform a get search for the pagination links using the query parameters title, author, genre and year
+router.get('/search', function (request, response, next) {
+    let pg = request.query.page;
+
+    if (!pg)
+        pg = 1;
+
+    let searchCriteria = [];
+
+    if (request.query.title != "") {
+        searchCriteria.push({
+            title: {
+                [Op.like]: '%' + request.query.title + '%'
+            }
+        });
+    }
+
+    if (request.query.author != "") {
+        searchCriteria.push({
+            author: {
+                [Op.like]: '%' + request.query.author + '%'
+            }
+        });
+    }
+
+    if (request.query.genre != "") {
+        searchCriteria.push({
+            genre: {
+                [Op.like]: '%' + request.query.genre + '%'
+            }
+        });
+    }
+
+    if (request.query.year != "") {
+        searchCriteria.push({ year: request.query.year });
+    }
+
+    Book.findAll({
+        order: [["createdAt", "DESC"]],
+        where: {
+            [Op.and]: searchCriteria
+        }
+    }).then(function (totalSearchBks) {
+        Book.findAll({
+            order: [["createdAt", "DESC"]],
+            where: {
+                [Op.and]: searchCriteria
+            },
+            offset: (pg * 8) - 8,
+            limit: 8
+        }).then(function (books) {
+            const totalPgs = Math.ceil(totalSearchBks.length / 8);
+            response.render("search", {
+                title: "Books", books: books, totalPgs: totalPgs,
+                searchTitle: request.query.title, searchAuthor: request.query.author, searchGenre: request.query.genre,
+                searchYear: request.query.year});
+        }).catch(function (err) {
+            console.log(err);
+            response.sendStatus(500);
+        });
+    });
+});
+
+// Route to show the book detail of the selected book
 router.get("/:id", function (request, response, next) {
     Book.findByPk(request.params.id).then(function (book) {
         if (book) {
@@ -56,6 +199,7 @@ router.get("/:id", function (request, response, next) {
     });
 });
 
+// Route to update the selected book
 router.post("/:id", function (request, response, next) {
     Book.findByPk(request.params.id).then(function (book) {
         if (book) {
@@ -83,7 +227,7 @@ router.post("/:id", function (request, response, next) {
     });
 });
 
-//Delete individual book 
+// Route to delete the selected book
 router.post("/:id/delete", function (request, response, next) {
     Book.findByPk(request.params.id).then(function(book){
         if(book) {
--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,5 @@ router.get('/', function(request, response, next) {
     response.redirect('/books');
 });
 
-/*router.get('/books', function (request, response, next) {
-    console.log("In books");
-    response.render("all_books");
-    
-    /*Book.findAll({order: [["createdAt", "DESC"]]}).then(function(books){
-      response.render("all_books", {books: books});
-    }).catch(function(err) {
-        response.send(500);
-    });
-});*/
-
 module.exports = router;
 
